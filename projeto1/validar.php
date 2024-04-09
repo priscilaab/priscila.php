bca03160241d408dd3e6ab772e92a16719727cb2
priscilabarbosa <149194766+priscilaab@users.noreply.github.com>
2024-04-09 16:56:44 -0300

    atualização2.09/04/2024

--- projeto1/validar.php
+++ projeto1/validar.php
@@ -1,7 +1,12 @@
 <?php
 
-echo "Seu username é: ".$_POST['username'];
-echo "<br>Sua senha é: ".$_POST['senha'];
+//Verificar se a página está sendo carregada por POST:
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "Seu username é: " . $_POST['username'];
+    echo "<br>Sua senha é: " . $_POST['senha'];
+} else {
+    echo "Essa página só pode ser carregada por POST.";
+}
 
 ?>
